56d4f55e55a19b5bfa3ace131c5dfc5067f3e2a7
EdenPark <edengoforit@gmail.com>
2022-10-25 08:02:08 +1300

    edit page back button

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NavController } from '@ionic/angular';
+import { Place } from '../../places.model';
+import { PlacesService } from './../../places.service';
 
 @Component({
   selector: 'app-edit-offer',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-offer.page.scss'],
 })
 export class EditOfferPage implements OnInit {
-
-  constructor() { }
+  place: Place;
+  constructor(
+    private route: ActivatedRoute,
+    private placesService: PlacesService,
+    private navCtrl: NavController
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/offers');
+      }
+      const id = +paramMap.get('placeId');
+      this.place = this.placesService.getPlace(id);
+    });
   }
-
 }
