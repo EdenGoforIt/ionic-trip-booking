481da688ac05d27efb3d38ab84b65b5346287eab
EdenPark <edengoforit@gmail.com>
2022-11-06 17:48:14 +1300

    move offer item into the component

---
+++ src/app/places/offers/offer-item/offer-item.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Place } from '../../places.model';
+
+@Component({
+  selector: 'app-offer-item',
+  templateUrl: './offer-item.component.html',
+  styleUrls: ['./offer-item.component.scss'],
+})
+export class OfferItemComponent implements OnInit {
+  @Input() offer: Place;
+  constructor() {}
+
+  ngOnInit() {}
+}
