b3333b5cc460c43ca83f1230f2c9d9f8fa04d20e
EdenPark <edengoforit@gmail.com>
2022-12-08 07:22:25 +1300

    delete booking method added

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -1,21 +1,28 @@
-import { AuthService } from './../auth/auth.service';
-import { BehaviorSubject } from 'rxjs';
-import { Booking } from './booking.model';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { delay, take, tap } from 'rxjs/operators';
+import { AuthService } from './../auth/auth.service';
+import { Booking } from './booking.model';
 
 @Injectable({ providedIn: 'root' })
 export class BookingService {
   private _bookings = new BehaviorSubject<Booking[]>([]);
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   get bookings() {
     return this._bookings.asObservable();
   }
 
-
-  delete(id: number): void {
-    // this._bookings = this._bookings.filter((x) => x.id !== id);
+  delete(id: number): Observable<any> {
+    return this.bookings.pipe(
+      take(1),
+      delay(1000),
+      tap((bookings) => {
+        const newBookings = bookings.filter((x) => x.id !== id);
+        this._bookings.next(bookings.concat(newBookings));
+      })
+    );
   }
   add(
     placeId: number,
@@ -27,7 +34,25 @@ export class BookingService {
     dateFrom: Date,
     dateTo: Date
   ) {
-    const newBooking = new Booking(Math.random(), placeId, this.authService._userId, placeTitle,
-      guestNumber, placeImage, firstName, lastName, dateFrom, dateTo);
+    const newBooking = new Booking(
+      Math.random(),
+      placeId,
+      this.authService._userId,
+      placeTitle,
+      guestNumber,
+      placeImage,
+      firstName,
+      lastName,
+      dateFrom,
+      dateTo
+    );
+
+    return this.bookings.pipe(
+      take(1),
+      delay(1000),
+      tap((bookings) => {
+        this._bookings.next(bookings.concat(newBooking));
+      })
+    );
   }
 }
--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,19 +1,30 @@
+import { takeUntil } from 'rxjs/operators';
 import { IonItemSliding } from '@ionic/angular';
 import { Booking } from './booking.model';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { BookingService } from './booking.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.page.html',
   styleUrls: ['./bookings.page.scss'],
 })
-export class BookingsPage implements OnInit {
+export class BookingsPage implements OnInit, OnDestroy {
   bookings: Booking[];
+  readonly destroy$ = new Subject<any>();
   constructor(private bookingService: BookingService) {}
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   ngOnInit() {
-    // this.bookings = this.bookingService.bookings;
+    this.bookingService.bookings
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((bookings) => {
+        this.bookings = bookings;
+      });
   }
   onDelete(bookingId: number, slidingBooking: IonItemSliding) {
     slidingBooking.close();
